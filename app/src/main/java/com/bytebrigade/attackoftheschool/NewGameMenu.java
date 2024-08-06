49c5a1fc9114c795e1593c51a1fabec66e5f27b0
Jason <willis509@gmail.com>
2024-08-05 20:17:35 -0400

    Added boolean to check profile name is valid. Profile name but not be black and be more than 2 characters.

--- app/src/main/java/com/bytebrigade/attackoftheschool/NewGameMenu.java
+++ app/src/main/java/com/bytebrigade/attackoftheschool/NewGameMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bytebrigade.attackoftheschool.gameplay.AssignmentScreen;
 import com.bytebrigade.attackoftheschool.gameplay.Profile;
@@ -26,18 +27,27 @@ public class NewGameMenu extends AppCompatActivity {
         startGameButton.setOnClickListener(v ->{
             // Capture the input from the user
             String characterName = characterNameInput.getText().toString();
-            // Update Profile class w/ name
-            Profile.profileName = characterName;
-
-
-            Intent intent = new Intent(NewGameMenu.this, AssignmentScreen.class);
-            startActivity(intent);
-            finish();
+            // Check profile name is valid and update Profile class w/ name
+            if (isProfileNameValid(characterName)) {
+                Profile.profileName = characterName;
+                Intent intent = new Intent(NewGameMenu.this, AssignmentScreen.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(NewGameMenu.this, "Please enter a valid character name.", Toast.LENGTH_SHORT).show();
+            }
         });
+
+
         cancelButton.setOnClickListener(v ->{
             Intent intent = new Intent(NewGameMenu.this, MainMenu.class);
             startActivity(intent);
             finish();
         });
+    }
+
+    private boolean isProfileNameValid(String profileName){
+        // Check if the name is not empty and has at least 3 characters
+        return profileName != null && !profileName.trim().isEmpty() && profileName.length() >= 3;
     }
 }
