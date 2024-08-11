c1af619ed355b94e1814bcd6f613d91527fc838c
Shawn Zen <SMZen@student.fullsail.edu>
2024-08-11 16:19:28 -0400

    Handled the click event of the "Credits" button within the Intro Screen to open the correct layout.

--- app/src/main/java/com/bytebrigade/attackoftheschool/MainMenuCreditsActivity.java
+++ app/src/main/java/com/bytebrigade/attackoftheschool/MainMenuCreditsActivity.java
@@ -1,6 +1,9 @@
 package com.bytebrigade.attackoftheschool;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainMenuCreditsActivity extends AppCompatActivity {
