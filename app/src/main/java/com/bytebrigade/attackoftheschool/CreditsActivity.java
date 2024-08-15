d85f70fd951db669b0ca464388cf94895a161dad
Shawn Zen <SMZen@student.fullsail.edu>
2024-08-14 20:30:32 -0400

    Created a "Back To Main Menu" button for the main menu credits screen as well as the end game credits screen and implemented functionality.

--- app/src/main/java/com/bytebrigade/attackoftheschool/CreditsActivity.java
+++ app/src/main/java/com/bytebrigade/attackoftheschool/CreditsActivity.java
@@ -1,13 +1,22 @@
 package com.bytebrigade.attackoftheschool;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CreditsActivity extends AppCompatActivity {
 
+    Button endGameBackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        endGameBackButton = findViewById(R.id.endGameBackButton);
+        endGameBackButton.setOnClickListener(v -> {
+            Intent intent = new Intent(CreditsActivity.this, MainMenu.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/bytebrigade/attackoftheschool/MainMenuCreditsActivity.java
+++ app/src/main/java/com/bytebrigade/attackoftheschool/MainMenuCreditsActivity.java
@@ -8,9 +8,18 @@ import android.os.Bundle;
 
 public class MainMenuCreditsActivity extends AppCompatActivity {
 
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_credits);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainMenuCreditsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
