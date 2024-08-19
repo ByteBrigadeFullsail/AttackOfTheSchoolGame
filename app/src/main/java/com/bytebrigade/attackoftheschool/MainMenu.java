f27894ea4d7082a0ba0062a8c98df2e869afcbba
Austin Miller <ALMiller4@student.fullsail.edu>
2024-08-19 18:48:05 -0500

    made a method for duplicate code to fix merge issues in github tried this again

--- app/src/main/java/com/bytebrigade/attackoftheschool/MainMenu.java
+++ app/src/main/java/com/bytebrigade/attackoftheschool/MainMenu.java
@@ -2,6 +2,7 @@ package com.bytebrigade.attackoftheschool;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +14,7 @@ public class MainMenu extends AppCompatActivity {
     Button newGameButton;
     Button continueButton;
     Button storeButton;
-    int x5Tracker;
-    int x2Tracker;
-    int x10Tracker;
+
 
 
 
@@ -39,34 +38,37 @@ public class MainMenu extends AppCompatActivity {
         continueButton.setOnClickListener(v ->{
             //continue button
         });
-        storeButton.setOnClickListener(v ->{
-            //Store button
-            Bundle x5 = getIntent().getExtras();
-
-            Intent intent = new Intent();
-            intent.setClass(MainMenu.this, StoreFunctionality.class);
-            intent.putExtra("Uniqid","From_Activity_A");
-            if(x5 != null)
-            {
-                String x5changetext= x5.getString("x5name");
-                String x2changetext= x5.getString("x2name");
-                String x10changetext= x5.getString("x10name");
-                intent.putExtra("x5",x5changetext);
-                intent.putExtra("x2",x2changetext);
-                intent.putExtra("x10",x10changetext);
-                x5Tracker = x5.getInt("x5Tracker");
-                x2Tracker = x5.getInt("x2Tracker");
-                x10Tracker = x5.getInt("x10Tracker");
-                intent.putExtra("x5Tracker",x5Tracker);
-                intent.putExtra("x2Tracker",x2Tracker);
-                intent.putExtra("x10Tracker",x10Tracker);
-
-            }
-            startActivity(intent);
-            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-            finish();
-
-        });
+        storeButton.setOnClickListener(this::onClick);
     }
 
+    private void onClick(View v) {
+        int x5Tracker;
+        int x2Tracker;
+        int x10Tracker;
+        //Store button
+        Bundle x5 = getIntent().getExtras();
+
+        Intent intent = new Intent();
+        intent.setClass(MainMenu.this, StoreFunctionality.class);
+        intent.putExtra("Uniqid", "From_Activity_A");
+        if (x5 != null) {
+            String x5changetext = x5.getString("x5name");
+            String x2changetext = x5.getString("x2name");
+            String x10changetext = x5.getString("x10name");
+            intent.putExtra("x5", x5changetext);
+            intent.putExtra("x2", x2changetext);
+            intent.putExtra("x10", x10changetext);
+            x5Tracker = x5.getInt("x5Tracker");
+            x2Tracker = x5.getInt("x2Tracker");
+            x10Tracker = x5.getInt("x10Tracker");
+            intent.putExtra("x5Tracker", x5Tracker);
+            intent.putExtra("x2Tracker", x2Tracker);
+            intent.putExtra("x10Tracker", x10Tracker);
+
+        }
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        finish();
+
+    }
 }
